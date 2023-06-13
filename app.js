5e9ae2e0a33358035a222aed9afe24df87806f6e
Ali Khan <merabhai786786@gmail.com>
2023-06-13 18:07:54 +0500

    creating an array of products

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const slidermain = document.querySelector(".slidermain");
 const manu_item= document.querySelectorAll(".manu-item");
 
+const product=[
+   {
+      id: 1,
+      title: "ARTEM",
+      price: 200,
+      colors:[
+         {
+            col: "burlywood",
+            img: "./Untitledm1.png"
+         },
+         {
+            col: "rgb(104, 177, 104)",
+            img: "./Untitledm1.png"
+         }
+      ]
+   }
+]
+
+
+
+
+
+
 
 manu_item.forEach((item, index)=>{
    item.addEventListener("click",()=>{
